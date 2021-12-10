d7b1c33d2bdd6df35c69a2d1d1d71b0c06b02708
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-10 03:32:59 +0000

    Completed printIndices function on arrays.js

--- arrays.js
+++ arrays.js
@@ -2,13 +2,53 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const item of items) {
+    console.log(`${item} ${items.indexOf(item)}`);
+  }
 }
+
+  // for (let i = 0; i < items.length; i += 1)
+
+// def print_indices(items):
+//     """Print each item in the list, followed by its index.
+
+//     The output should look like this:
+//         apple 0
+//         berry 1
+//         cherry 2
+
+//     Arguments:
+//         items (list)
+
+//     Returns:
+//         None
+//     """
+
+//     for i in range(len(items)):
+//         print(f'{items[i]} {i}')
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
 }
+
+
+// def every_other_item(items):
+//     """Print a list of every other item in `items`
+
+//     Start with index 0.
+
+//     Arguments:
+//         items (list)
+//     """
+
+//     result = []
+
+//     for i in range(len(items)):
+//         if i % 2 == 0:
+//             result.append(items[i])
+
+//     print(result)
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
