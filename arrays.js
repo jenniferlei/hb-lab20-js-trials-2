4a18dcd950cc8761088cb674a2e50865192c264b
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-10 03:50:33 +0000

    Completed arrays.js

--- arrays.js
+++ arrays.js
@@ -7,50 +7,27 @@ function printIndices(items) {
   }
 }
 
-  // for (let i = 0; i < items.length; i += 1)
-
-// def print_indices(items):
-//     """Print each item in the list, followed by its index.
-
-//     The output should look like this:
-//         apple 0
-//         berry 1
-//         cherry 2
-
-//     Arguments:
-//         items (list)
-
-//     Returns:
-//         None
-//     """
-
-//     for i in range(len(items)):
-//         print(f'{items[i]} {i}')
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const resultitems = [];
+
+  for (const i in items) {
+    if (i % 2 === 0) {
+      resultitems.push(items[i]);
+    }
+  }
+  console.log(resultitems);
 }
+everyOtherItem(['apple','berry','cherry'])
 
 
-// def every_other_item(items):
-//     """Print a list of every other item in `items`
-
-//     Start with index 0.
-
-//     Arguments:
-//         items (list)
-//     """
-
-//     result = []
-
-//     for i in range(len(items)):
-//         if i % 2 == 0:
-//             result.append(items[i])
-
-//     print(result)
-
-// 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortednItems = items.sort((a, b) => a - b).slice(0, n);
+  sortednItems.reverse();
+
+  console.log(sortednItems);
+  
 }
+
+smallestNItems([1, 30, 4, 21, 100000], 2)
