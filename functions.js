16c6e6722d6d9fff72dc95dad6ae9af26afc309f
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-10 04:11:36 +0000

    Completed functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,52 @@
 'use strict';
 
 // 1. isHometown
+// function isHometown(town) {
+//     return town === "San Francisco";
+// }
 
-// Define your function here
+const isHometown = town => {
+    return town === "San Francisco";
+};
+
+console.log(isHometown("San Francisco"));
+console.log(isHometown("San Jose"));
+
+
 
 // 2. getFullName
+// function getFullName(firstname, lastname) {
+//     return `${firstname} ${lastname}`;
+// }
 
-// Define your function here
+const getFullName = (firstname, lastname) => {
+    return `${firstname} ${lastname}`;
+};
+
+console.log(getFullName('Balloonicorn', 'Balloon'));
+
 
 // 3. calculateTotal
+// function calculateTotal(basePrice, state, tax = 0.05) {
+//     const subtotal = basePrice * (1 + tax)
+// }
 
-// Define your function here
+const calculateTotal = (basePrice, state, tax = 0.05) => {
+    const subtotal = basePrice * (1 + tax);
+
+    let fee = 0;
+    if (state === "CA") {
+        fee = 0.03 * subtotal;
+    } else if (state === "PA") {
+        fee = 2;
+    } else if (state === "MA") {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+    return subtotal + fee;
+};
+
+console.log(calculateTotal(1, 'CA'));
